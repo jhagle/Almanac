287a3c1504a6ac2dfc198a982b51bae3eb0867d0
James Hagle <rob.hagle@gmail.com>
2015-03-13 15:01:34 -0500

    testing settings

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -1,5 +1,11 @@
 # Django settings for mysite project.
 import os
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -13,18 +19,18 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/home/ubuntu/venv/mysite/mysite.db',                      # Or path to database file if using sqlite3.
+        'NAME': '/venv/mysite/mysite.db',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
-        'HOST': 'shielded-ocean-8208.herokuapp.com',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'HOST': 'http://pacific-beyond-1390.herokuapp.com/',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
 }
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
