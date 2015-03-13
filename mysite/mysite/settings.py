d88d133dc22df91fc798d322f5e4eb76842b73e7
James Hagle <rob.hagle@gmail.com>
2015-03-13 15:08:14 -0500

    test

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -3,9 +3,13 @@ import os
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
 
+<<<<<<< HEAD
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
+=======
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+>>>>>>> parent of 1573ca9... Testing new settings
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -19,11 +23,19 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+<<<<<<< HEAD
         'NAME': '/venv/mysite/mysite.db',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
         'HOST': 'http://pacific-beyond-1390.herokuapp.com/',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+=======
+        'NAME': 'mysite.db',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+>>>>>>> parent of 1573ca9... Testing new settings
         'PORT': '',                      # Set to empty string for default.
     }
 }
@@ -68,7 +80,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = 'staticfiles'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -76,7 +88,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    "C:/Users/H/Desktop/mysite/static",
+    os.path.join(BASE_DIR, 'static'),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -136,16 +148,17 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'blog',
     'haystack',
+    'whoosh',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
 
+# Search Haystack
 HAYSTACK_CONNECTIONS = {
     'default': {
         'ENGINE': 'haystack.backends.whoosh_backend.WhooshEngine',
         'PATH': os.path.join(os.path.dirname(__file__), 'whoosh_index'),
-    }
-    
+    },
 }
 
 # A sample logging configuration. The only tangible logging
@@ -176,3 +189,4 @@ LOGGING = {
         },
     }
 }
+
